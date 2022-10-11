9dddcf5f9dd7f75fcfdef35e869555b2abd497e1
James Paul Tulod <paultulod@pm.me>
2022-10-11 11:59:41 +0800

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (arr, ...num) {
+  return arr.filter(x => !num.includes(x))
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
