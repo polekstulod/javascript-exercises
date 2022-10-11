5bb7de5298119c6270a4fbbddf67fe6b6e7ee404
James Paul Tulod <paultulod@pm.me>
2022-10-11 15:41:11 +0800

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') return 'ERROR'
 
-};
+  if (min > max) max = [min, (min = max)][0]
+
+  let sum = 0
+  Array.from({ length: max }, (_, i) => i + min).forEach(x => (sum += x))
+
+  return sum
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
