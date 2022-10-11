2b930bad8f5807ad6057dd4a737395f64adb8779
James Paul Tulod <paultulod@pm.me>
2022-10-11 16:29:00 +0800

    Finish 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 100 !== 0) {
+    if (year % 4 === 0) {
+      return true
+    }
+    return false
+  }
+  if (year % 400 === 0) return true
 
-};
+  return false
+}
+
+console.log(leapYears(1600))
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
