94b610911df0df6a377e56f3763a992066344e06
James Paul Tulod <paultulod@pm.me>
2022-10-18 22:52:15 +0800

    Finish Exercise 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,26 @@
-const add = function() {
-	
-};
+const add = function (a, b) {
+  return a + b
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (a, b) {
+  return a - b
+}
 
-const sum = function() {
-	
-};
+const sum = function (arr) {
+  return arr.reduce((prev, curr) => prev + curr, 0)
+}
 
-const multiply = function() {
+const multiply = function (arr) {
+  return arr.reduce((prev, curr) => prev * curr, 1)
+}
 
-};
+const power = function (a, b) {
+  return a ** b
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function (val) {
+  return Array.from(Array(val), (_, i) => i + 1).reduce((prev, curr) => prev * curr, 1)
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +29,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
-};
+  factorial,
+}
