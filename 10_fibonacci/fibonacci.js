704a113fd30ea48640008d0571fafd6f1058f8d8
James Paul Tulod <paultulod@pm.me>
2022-10-19 11:26:33 +0800

    Finish Exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (n) {
+  if (+n < 0) return 'OOPS'
+  if (+n === 0 || +n === 1) return +n
+  else return fibonacci(+n - 1) + fibonacci(+n - 2)
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
