3ef791fcd29a4026d8f1caf34eb774a44f19c371
James Paul Tulod <paultulod@pm.me>
2022-10-19 09:24:54 +0800

    Finish Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (x) {
+  const arr = x
+    .toLowerCase()
+    .split('')
+    .filter(letter => isLetter(letter))
+    .reverse()
 
-};
+  return arr.toString() === arr.reverse().toString()
+}
+
+function isLetter(c) {
+  return c.toLowerCase() != c.toUpperCase()
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
